66237a3f0f9575de0053d19c8140a10d543f97d5
areintha <areintha@student.codam.nl>
2021-06-19 16:25:10 +0200

    rush pdf and some minor fixes

--- c02/ex04/ft_str_is_lowercase.c
+++ c02/ex04/ft_str_is_lowercase.c
@@ -6,7 +6,7 @@
 /*   By: areintha <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/06/17 14:04:25 by areintha      #+#    #+#                 */
-/*   Updated: 2021/06/17 14:15:06 by areintha      ########   odam.nl         */
+/*   Updated: 2021/06/18 14:32:59 by areintha      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@ int	ft_str_is_lowercase(char *str);
 
 int	ft_str_is_lowercase(char *str)
 {
-	char	r ;
+	int	r ;
 
 	if (str[0] == '\0')
-		r = '1' ;
+		r = 1 ;
 	else
 	{
 		while (*str)
 		{
 			if (*str >= 'a' && *str <= 'z')
-				r = '0';
+				r = 1;
 			else
-				r = '1';
+			{	
+				r = 0 ;
+				break ;
+			}	
 			str++ ;
 		}
 	}
